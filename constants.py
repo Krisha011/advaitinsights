5d1746af20d0f4d2f397e208e94e41b3a6a73168
Krisha011 <24pai024@sxca.edu.in>
2025-05-28 20:55:34 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,62 @@
+# constants.py
+
+# State list (can be extended later)
+STATE_LIST = [
+    "Bihar - Patna"
+]
+
+# Advait Theragnostics - Services and Solutions
+ADVAIT_SERVICES = [
+    "MALDI-TOF Microbial ID",
+    "Sanger Sequencing",
+    "Whole Genome Sequencing (WGS)",
+    "BiomeView Focus (Targeted NGS)",
+    "BiomeView Spectrum (Metagenomics NGS)",
+    "Species Assay",
+    "Resistance Gene Detection",
+    "mRNA Gene Expression Profiling",
+    "Discovery Metabolomics",
+    "Targeted Metabolomics Assays",
+    "Therapeutic Drug Monitoring (TDM)",
+    "Global Proteomics Profiling",
+    "4D Proteomics (timsTOF-based)",
+    "Plasma Proteomics",
+    "Exosome Proteomics",
+    "Protein Characterization",
+    "Genotyping",
+    "Pharmacogenomics (PGx)",
+    "Tumor Profiling",
+    "Liquid Biopsy",
+    "Gut Microbiome + Metabolomics",
+    "Vaginal Microbiome + Metabolomics",
+    "Multiomics Integration",
+    "Fecal Metabolomics",
+    "Cell Culture Metabolomics",
+    "Fungal Metabolomics",
+    "Bacterial Metabolomics"
+]
+
+# Funding Agencies
+FUNDING_AGENCIES = [
+    "Ministry of Health & Family Welfare (MoHFW)",
+    "ICMR (Indian Council of Medical Research)",
+    "DST (Department of Science & Technology)",
+    "DBT (Department of Biotechnology)",
+    "CSIR (Council of Scientific and Industrial Research)",
+    "AYUSH (Ayurveda, Yoga, Unani, Siddha, Homeopathy)",
+    "ICAR (Indian Council of Agricultural Research)",
+    "SERB (Science and Engineering Research Board)",
+    "BIRAC (Biotechnology Industry Research Assistance Council)",
+    "DBT-Wellcome Trust India Alliance",
+    "NHM (National Health Mission)",
+    "NACO (National AIDS Control Organisation)",
+    "DRDO / DRDE (Defence Research & Development Organisation / Defence Research & Development Establishment)",
+    "ICFRE (Indian Council of Forestry Research and Education)",
+    "MoEFCC (Ministry of Environment, Forest and Climate Change)",
+    "ISRO (Indian Space Research Organisation)",
+    "DAE / BARC (Dept. of Atomic Energy / Bhabha Atomic Research Centre)",
+    "NIHFW (National Institute of Health and Family Welfare)",
+    "ICSSR (Indian Council of Social Science Research)",
+    "RGCB (Rajiv Gandhi Centre for Biotechnology)",
+    "TDB (Technology Development Board – DST)"
+]
